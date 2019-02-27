288ae59f929bc0d07835ad534cadcbabb2b0bcb8
Naoki Takezoe <takezoe@gmail.com>
2019-02-27 15:08:15 +0900

    Add Paralellizer#repeat()

--- src/main/scala/com/github/takezoe/parallelizer/Worker.scala
+++ src/main/scala/com/github/takezoe/parallelizer/Worker.scala
@@ -7,17 +7,14 @@ import scala.util.Try
 
 private[parallelizer] class Worker[T, R](
   requestQueue: BlockingQueue[Worker[T, R]],
-  resultQueue: BlockingQueue[Option[Try[R]]],
+  resultQueue: BlockingQueue[Option[R]],
   f: T => R) extends Runnable {
 
   val message: AtomicReference[T] = new AtomicReference[T]()
 
   override def run: Unit = {
     try {
-      val t = Try {
-        f(message.get())
-      }
-      resultQueue.put(Some(t))
+      resultQueue.put(Some(f(message.get())))
     } finally {
       requestQueue.put(this)
     }
@@ -26,7 +23,7 @@ private[parallelizer] class Worker[T, R](
 
 private[parallelizer] class WithIndexWorker[T, R](
   requestQueue: BlockingQueue[WithIndexWorker[T, R]],
-  resultArray: Array[Try[R]],
+  resultArray: Array[R],
   f: T => R) extends Runnable {
 
   val message: AtomicReference[(T, Int)] = new AtomicReference[(T, Int)]()
@@ -34,10 +31,7 @@ private[parallelizer] class WithIndexWorker[T, R](
   override def run: Unit = {
     try {
       val (m, i) = message.get()
-      val t = Try {
-        f(m)
-      }
-      resultArray(i) = t
+      resultArray(i) = f(m)
     } finally {
       requestQueue.put(this)
     }
