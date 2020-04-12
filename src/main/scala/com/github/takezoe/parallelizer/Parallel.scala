77025697fed3963bcc94f373bbc5ea597fd1fcd4
Naoki Takezoe <takezoe@gmail.com>
2020-04-12 19:47:14 +0900

    Support escaping from Parallel immediately

--- src/main/scala/com/github/takezoe/parallelizer/Parallel.scala
+++ src/main/scala/com/github/takezoe/parallelizer/Parallel.scala
@@ -5,46 +5,57 @@ import java.util.concurrent.{ExecutorService, Executors, LinkedBlockingQueue, Ti
 
 import scala.concurrent.duration.Duration
 import scala.reflect.ClassTag
+import scala.collection.JavaConverters._
+import scala.util.control.ControlThrowable
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.atomic.AtomicReference
 
 /**
- * Provides tiny utilities for parallelization.
- *
- * For example, each element of source is proceeded in parallel in the following example.
- *
- * {{{
- * val source: Seq[Int] = Seq(1, 2, 3)
- * val result: Seq[Int] = Parallelizer.run(source){ i =>
- *   ...
- * }
- * }}}
- *
- * Parallelism can be specified as a second parameter. The default value is a number of available processors.
- *
- * {{{
- * val result: Seq[Int] = Parallelizer.run(source, 100){ i =>
- *   ...
- * }
- * }}}
- */
+  * Provides tiny utilities for parallelization.
+  *
+  * For example, each element of source is proceeded in parallel in the following example.
+  *
+  * {{{
+  * val source: Seq[Int] = Seq(1, 2, 3)
+  * val result: Seq[Int] = Parallelizer.run(source){ i =>
+  *   ...
+  * }
+  * }}}
+  *
+  * Parallelism can be specified as a second parameter. The default value is a number of available processors.
+  *
+  * {{{
+  * val result: Seq[Int] = Parallelizer.run(source, 100){ i =>
+  *   ...
+  * }
+  * }}}
+  */
 object Parallel {
 
+  private class BreakException extends ControlThrowable
+
+  // Parallel execution can be stopped by calling this method.
+  def break: Unit = throw new BreakException()
+
   /**
-   * Process all elements of the source by the given function then wait for completion.
-   *
-   * @param source Source collection
-   * @param parallelism Parallelism, the default value is a number of available processors
-   * @param f Function which process each element of the source collection
-   * @return Collection of results
-   */
-  def run[T, R: ClassTag](source: Seq[T],
-    parallelism: Int = Runtime.getRuntime.availableProcessors(),
-    timeout: Duration = Duration.Inf
+    * Process all elements of the source by the given function then wait for completion.
+    *
+    * @param source Source collection
+    * @param parallelism Parallelism, the default value is a number of available processors
+    * @param f Function which process each element of the source collection
+    * @return Collection of results
+    */
+  def run[T, R: ClassTag](
+      source: Seq[T],
+      parallelism: Int = Runtime.getRuntime.availableProcessors(),
+      timeout: Duration = Duration.Inf
   )(f: T => R): Seq[R] = {
     val requestQueue = new LinkedBlockingQueue[Worker[T, R]](parallelism)
-    val resultArray = new Array[R](source.length)
+    val resultQueue  = new LinkedBlockingQueue[R]()
+    val interrupted  = new AtomicBoolean(false)
 
     Range(0, parallelism).foreach { _ =>
-      val worker = new Worker[T, R](requestQueue, resultArray, f)
+      val worker = new Worker[T, R](requestQueue, resultQueue, interrupted, f)
       requestQueue.put(worker)
     }
 
@@ -52,15 +63,19 @@ object Parallel {
 
     try {
       // Process all elements of source
-      val it = source.zipWithIndex.iterator
-      while(it.hasNext) {
+      val it = source.iterator
+      while (it.hasNext && !interrupted.get()) {
         val worker = requestQueue.take()
-        worker.message.set(it.next())
-        executor.execute(worker)
+        if (!interrupted.get()) {
+          worker.message.set(it.next())
+          executor.execute(worker)
+        } else {
+          requestQueue.put(worker)
+        }
       }
 
       // Wait for completion
-      while(requestQueue.size() != parallelism){
+      while (requestQueue.size() != parallelism) {
         try {
           Thread.sleep(10)
         } catch {
@@ -68,8 +83,7 @@ object Parallel {
         }
       }
 
-      resultArray.toSeq
-
+      resultQueue.asScala.toSeq
     } catch {
       case _: InterruptedException => throw new TimeoutException()
 
@@ -80,4 +94,24 @@ object Parallel {
     }
   }
 
+  private class Worker[T, R](
+      requestQueue: BlockingQueue[Worker[T, R]],
+      resultQueue: BlockingQueue[R],
+      interrupted: AtomicBoolean,
+      f: T => R
+  ) extends Runnable {
+
+    val message: AtomicReference[T] = new AtomicReference[T]()
+
+    override def run: Unit = {
+      try {
+        resultQueue.put(f(message.get()))
+      } catch {
+        case _: BreakException =>
+          interrupted.set(true)
+      } finally {
+        requestQueue.put(this)
+      }
+    }
+  }
 }
--- src/test/scala/com/github/takezoe/parallelizer/ParallelSpec.scala
+++ src/test/scala/com/github/takezoe/parallelizer/ParallelSpec.scala
@@ -6,13 +6,12 @@ import scala.language.postfixOps
 import scala.util.{Success, Failure}
 import scala.util.Try
 
-
 class ParallelSpec extends FunSuite {
 
-  test("run()"){
+  test("run()") {
     val source = Seq(1, 2, 3)
-    val start = System.currentTimeMillis()
-    val result = Parallel.run(source, parallelism = 2){ i =>
+    val start  = System.currentTimeMillis()
+    val result = Parallel.run(source, parallelism = 2) { i =>
       Thread.sleep(500)
       i * 2
     }
@@ -22,33 +21,59 @@ class ParallelSpec extends FunSuite {
     assert(result == List(2, 4, 6))
   }
 
-  test("Seq.parallelMap()"){
+  test("Seq.parallelMap()") {
     val source = Seq(1, 2, 3)
-    val start = System.currentTimeMillis()
-    val result = source.parallelMap(parallelism = 2){ i =>
+    val start  = System.currentTimeMillis()
+    val result = source.parallelMap(parallelism = 2) { i =>
       Thread.sleep(500)
       i * 2
     }
     val duration = System.currentTimeMillis() - start
 
     assert(duration > 500 && duration < 1100)
-    assert(result == List(2, 4, 6))
+    assert(result.sorted == Seq(2, 4, 6))
   }
 
-  test("failure in run()"){
-    val source = Seq(1, 2, 3)
+  test("unproceeded failure in run()") {
+    val source    = Seq(1, 2, 3)
     val exception = new RuntimeException("failure")
 
-    val result = Parallel.run(source, parallelism = 2){ i =>
-      Try {
-        if(i == 2){
+    val result = Parallel.run(source, parallelism = 2) { i =>
+      if (i == 2) {
+        throw exception
+      }
+      i * 2
+    }
+
+    assert(result.sorted == Seq(2, 6))
+  }
+
+  test("failure in run()") {
+    val source    = Seq(1, 2, 3)
+    val exception = new RuntimeException("failure")
+
+    val result = Parallel.run(source, parallelism = 2) { i =>
+      (i, Try {
+        if (i == 2) {
           throw exception
         }
         i * 2
-      }
+      })
     }
 
-    assert(result == List(Success(2), Failure(exception), Success(6)))
+    assert(result.sortBy(_._1).map(_._2) == Seq(Success(2), Failure(exception), Success(6)))
+  }
+
+  test("break execution") {
+    val source = Seq(1, 2, 3)
+    val result = Parallel.run(source, parallelism = 1) { i =>
+      if (i == 2) {
+        Parallel.break
+      }
+      i
+    }
+
+    assert(result == Seq(1))
   }
 
 }
